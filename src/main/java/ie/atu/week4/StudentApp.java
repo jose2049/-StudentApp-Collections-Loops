ed2c44bb5c7e6c26457bd2f427d98f5c7ef9393e
Jose Alvarez <g00463862@atu.ie>
2025-10-12 19:42:07 +0100

    added a list to store the different objects and manage to print all the info gathered by the user

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -1,6 +1,7 @@
 package ie.atu.week4;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentApp {
@@ -8,22 +9,32 @@ public class StudentApp {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        List<StudentClass> studentClassList = new ArrayList<StudentClass>();
 
+        //Asking the user how many users wants to add
         System.out.println("How many students do you want to enter?");
         int count = sc.nextInt();
 
         int i = 0;
 
         while (i < count) {
-            StudentClass s = new StudentClass();
+            StudentClass s = new StudentClass();            //creating an instance of a class called 's'
 
+            //Here I ask the user to enter all the data which it's scanned and stored away in the instance
             System.out.println("Please enter your name: ");
             s.setName(new Scanner(System.in).nextLine());
             System.out.println("Please enter your email: ");
             s.setEmail(new Scanner(System.in).nextLine());
             System.out.println("Please enter your course name: ");
-            s.setCourse(new Scanner(System.in).nextLine());
+            s.setCourse(new Scanner(System.in).nextLine());         //Adding every instance to the List
+            studentClassList.add(s);
             i++;
         }
+
+        //Printing out all students with all the info gathered
+        for (StudentClass s: studentClassList) {
+            System.out.println(s);
+        }
+
     }
 }
--- src/main/java/ie/atu/week4/StudentClass.java
+++ src/main/java/ie/atu/week4/StudentClass.java
@@ -4,33 +4,49 @@ import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//Here I create the class
 public class StudentClass {
     private String name;
     private String email;
     private String course;
 
+    //Default Constructor
+    public StudentClass(){
+        System.out.println("StudentClass()");
+        this.name = "";
+        this.email = "";
+        this.course = "";
+    }
+
     public String getName() {
         return name;
-    }
+    }       //Here I get the name and return it when call it
 
     public void setName(String name) {
         this.name = name;
-    }
+    } //Here I update name with a new value
 
     public String getEmail() {
         return email;
-    }
+    }   //Here I get the email and return it when call it
 
     public void setEmail(String email) {
         this.email = email;
-    }
+    }   //Here I update the email with a new value
 
     public String getCourse() {
         return course;
-    }
+    }   //Here I get the course name and return it when call it
 
-    public void setCourse(String course) {
-        this.course = course;
+    public void setCourse(String course) { this.course = course; }  //Here I update the course name with a new value
 
+    @Override       //This line builds and returns a string containing the values of name, email and course
+    public String toString() {
+        return "StudentClass{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", course='" + course + '\'' +
+                '}';
     }
 }
